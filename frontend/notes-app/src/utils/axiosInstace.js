78e0b914ba0045a55168b254496b020ee736c915
MHD NOUR EDDIN BASSAL <119541895+MhdNour24@users.noreply.github.com>
2024-09-25 10:48:21 +0300

    Update axiosInstace.js

--- frontend/notes-app/src/utils/axiosInstace.js
+++ frontend/notes-app/src/utils/axiosInstace.js
@@ -1,5 +1,6 @@
 // axios instance
 import axios from "axios";
+import store from "../store"; // تأكد من تعديل هذا حسب مسار store الخاص بك
 
 const axiosInstance = axios.create({
   baseURL: "https://notes-app-backend-k0t1.onrender.com",  
@@ -8,11 +9,10 @@ const axiosInstance = axios.create({
     "content-type": "application/json",
   },
 });
- 
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const accessToken = localStorage.getItem("token");
+    const accessToken = store.getState().notes.token; // استخدام store.getState()
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
