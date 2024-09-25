c5b767f50d12f926654a68a030da3111124297f5
MHD NOUR EDDIN BASSAL <119541895+MhdNour24@users.noreply.github.com>
2024-09-25 10:46:02 +0300

    Update store.jsx

--- frontend/notes-app/src/store.jsx
+++ frontend/notes-app/src/store.jsx
@@ -1,9 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit'
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist"
+import storage from "redux-persist/lib/storage";
 
 // import the needed slices
 import NoteSlicesReducer from './features/NoteSlices'
+
+const persistConfig = { key: "root", storage, version: 1 };
+
+
+const persistedReducer  = persistReducer(persistConfig, NoteSlicesReducer);
+
+
+
 export default configureStore({
   reducer: {
-    notes:NoteSlicesReducer
-  }
+    notes:persistedReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+
